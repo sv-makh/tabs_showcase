031366a4d6ad9619f091d0c504a4a87d5dea292a
sv-makh <mithriam@mail.ru>
2023-08-17 20:45:28 +0300

    add dropdown menu to move to tabs

--- lib/pages/contained_tab_bar_page.dart
+++ lib/pages/contained_tab_bar_page.dart
@@ -10,52 +10,92 @@ class ContainedTabBarPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    double tabTitleWidth = MediaQuery.of(context).size.width/tabTitles.length - 16;
+    double tabTitleWidth =
+        MediaQuery
+            .of(context)
+            .size
+            .width / tabTitles.length - 16;
     double tabTitleHeight = 60;
+
+    GlobalKey<ContainedTabBarViewState> _key = GlobalKey();
+
+    String initialValue = tabTitles[0];
 
     return Scaffold(
       appBar: AppBar(
         title: Text('contained_tab_bar_view_with_custom_page_navigator'),
       ),
-      body: ContainedTabBarView(
-        tabs: [
-          ...tabTitles
-              .map(
-                (e) => Tooltip(
-                  message: e,
-                  child: Container(
-                    width: tabTitleWidth,
-                    height: tabTitleHeight,
-                    padding: EdgeInsets.only(left: 4, right: 4),
-                    decoration: BoxDecoration(
-                      color: Colors.transparent,
-                      border: Border.all(color: Colors.grey[600]!),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(4.0),
+      body: Column(
+        children: [
+          DropdownButton<String>(
+            value: initialValue,
+            items: tabTitles.map((String value) {
+              return DropdownMenuItem<String>(
+                value: value,
+                child: Text(value, overflow: TextOverflow.ellipsis,),
+              );
+            }).toList(),
+            onChanged: (value) {
+              initialValue = value!;
+              _key.currentState?.animateTo(tabTitles.indexOf(value));
+            },),
+          Container(
+            height: MediaQuery.of(context).size.height - 200,
+            child: ContainedTabBarView(
+              key: _key,
+              tabs: [
+                ...tabTitles
+                    .map(
+                      (e) =>
+                      Tooltip(
+                        message: e,
+                        child: Container(
+                          width: tabTitleWidth,
+                          height: tabTitleHeight,
+                          padding: EdgeInsets.only(left: 4, right: 4),
+                          decoration: BoxDecoration(
+                            color: Colors.transparent,
+                            border: Border.all(color: Colors.grey[600]!),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(4.0),
+                            ),
+                          ),
+                          child: Center(
+                              child: Text(
+                                e,
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              )),
+                        ),
                       ),
-                    ),
-                    child: Center(child: Text(e, overflow: TextOverflow.ellipsis, maxLines: 1,)),
-                  ),
+                )
+                    .toList(),
+              ],
+              tabBarProperties: TabBarProperties(
+                width: MediaQuery
+                    .of(context)
+                    .size
+                    .width,
+                height: tabTitleHeight + 10,
+                position: TabBarPosition.top,
+                background: Container(
+                  color: Colors.lightBlueAccent,
                 ),
-              )
-              .toList(),
-        ],
-        tabBarProperties: TabBarProperties(
-          width: MediaQuery.of(context).size.width,
-          height: tabTitleHeight + 10,
-          position: TabBarPosition.top,
-          background: Container(color: Colors.lightBlueAccent,),
-          labelColor: Colors.white,
-          indicator: ContainerTabIndicator(
-            width: tabTitleWidth,
-            height: tabTitleHeight,
-            radius: BorderRadius.circular(4.0),
-            color: Colors.blue,
+                labelColor: Colors.white,
+                indicator: ContainerTabIndicator(
+                  width: tabTitleWidth,
+                  height: tabTitleHeight,
+                  radius: BorderRadius.circular(4.0),
+                  color: Colors.blue,
+                ),
+              ),
+              views: tabViews,
+              onChange: (index) => print(index),
+            ),
           ),
-        ),
-        views: tabViews,
-        onChange: (index) => print(index),
-      ),
+        ],
+      )
+      ,
     );
   }
 }
